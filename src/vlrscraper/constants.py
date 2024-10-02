1b2dfcd1ec4dbc2b193019db6839c76dba5268a2
Joe <joantpat@gmail.com>
2024-10-02 16:25:43 +0100

    Added player inactive check

--- src/vlrscraper/constants.py
+++ src/vlrscraper/constants.py
@@ -5,3 +5,4 @@ PLAYER_DISPLAYNAME = xpath("h1", class_="wf-title")
 PLAYER_FULLNAME = xpath("h2", class_="player-real-name")
 PLAYER_IMAGE_SRC = join(xpath("div", class_="wf-avatar"), "img")
 PLAYER_CURRENT_TEAM = f"({xpath('a', class_='wf-module-item mod-first')})[1]"
+PLAYER_INACTIVE_CHECK = "(//a[contains(@class, 'wf-module-item mod-first')])[1]//div[contains(@class, 'ge-text-light')]"
