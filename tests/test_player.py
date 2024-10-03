0a79ce172f08ca409d544fe5670f40ecc3e4e5e0
Joe <joantpat@gmail.com>
2024-10-03 16:56:30 +0100

    More player tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,12 +1,29 @@
+import pytest
+
 from vlrscraper.player import Player, PlayerStatus
 from vlrscraper.team import Team
 
 
 def test_player_init():
+    # Correct exceptions
+    with pytest.raises(ValueError):
+        Player(0, None, None, None, None, None, None)
+
+    with pytest.raises(ValueError):
+        Player(-100, None, None, None, None, None, None)
+
+    with pytest.raises(ValueError):
+        Player("4004", None, None, None, None, None, None)
+
+    with pytest.raises(ValueError):
+        Player(None, None, None, None, None, None, None)
+
     benjy = Player(
         29873,
         "benjyfishy",
-        1001,
+        Team.from_player_page(
+            1001, "Team Heretics", "https://owcdn.net/img/637b755224c12.png"
+        ),
         "Benjamin",
         "Fish",
         "https://owcdn.net/img/665b77ca4bc4d.png",
@@ -14,10 +31,22 @@ def test_player_init():
     )
     assert benjy.get_id() == 29873
     assert benjy.get_display_name() == "benjyfishy"
-    assert benjy.get_current_team() == 1001
+    assert benjy.get_current_team().is_same_team(
+        Team.from_player_page(
+            1001, "Team Heretics", "https://owcdn.net/img/637b755224c12.png"
+        )
+    )
     assert benjy.get_name() == "Benjamin Fish"
     assert benjy.get_image() == "https://owcdn.net/img/665b77ca4bc4d.png"
     assert benjy.get_status() == PlayerStatus.ACTIVE
+
+    # No forename OR surname
+    crappy = Player(31207, None, None, None, None, None, None)
+    assert crappy.get_name() is None
+
+    # Forename but no surname
+    crappy = Player(31207, None, None, "Lee", None, None, None)
+    assert crappy.get_name() == "Lee"
 
 
 def test_player_equals():
@@ -30,8 +59,8 @@ def test_player_equals():
         "https://owcdn.net/img/665b77ca4bc4d.png",
         PlayerStatus.ACTIVE,
     )
-    assert benjy == benjy
-    assert benjy != 1
+    assert benjy.is_same_player(benjy)
+    assert not benjy.is_same_player(1)
 
     benjy2 = Player(
         298731,
@@ -43,7 +72,20 @@ def test_player_equals():
         PlayerStatus.ACTIVE,
     )
 
-    assert benjy != benjy2
+    # Different ID, different object
+    assert not benjy.is_same_player(benjy2)
+
+    # Different object, same ID
+    benjy3 = Player(
+        29873,
+        "benjyfishy",
+        1001,
+        "Benjamin",
+        "Fish",
+        "https://owcdn.net/img/665b77ca4bc4d.png",
+        PlayerStatus.ACTIVE,
+    )
+    assert benjy.is_same_player(benjy3)
 
 
 def test_string():
@@ -58,7 +100,7 @@ def test_string():
     )
     assert (
         str(benjy)
-        == "benjyfishy (Benjamin Fish) [https://owcdn.net/img/665b77ca4bc4d.png]"
+        == "Player(29873, benjyfishy, Benjamin Fish, https://owcdn.net/img/665b77ca4bc4d.png, 1001, ACTIVE)"
     )
 
 
@@ -67,26 +109,25 @@ def test_player_get():
     benjy = Player.get_player(29873)
     assert benjy.get_id() == 29873
     assert benjy.get_display_name() == "benjyfishy"
-    assert benjy.get_current_team() == Team.from_player_page(
-        1001, "Team Heretics", "https://owcdn.net/img/637b755224c12.png"
+    assert benjy.get_current_team().is_same_team(
+        Team.from_player_page(
+            1001, "Team Heretics", "https://owcdn.net/img/637b755224c12.png"
+        )
     )
     assert benjy.get_name() == "Benjamin Fish"
     assert benjy.get_image() == "https://owcdn.net/img/665b77ca4bc4d.png"
     assert benjy.get_status() == PlayerStatus.ACTIVE
-    assert benjy.is_fully_scraped() is True
 
     # Player with non-latin characters in name
     crappy = Player.get_player(31207)
     assert crappy.get_id() == 31207
     assert crappy.get_display_name() == "Carpe"
-    assert crappy.get_current_team() == Team.from_player_page(
-        14, "T1", "https://owcdn.net/img/62fe0b8f6b084.png"
+    assert crappy.get_current_team().is_same_team(
+        Team.from_player_page(14, "T1", "https://owcdn.net/img/62fe0b8f6b084.png")
     )
-    assert crappy.get_current_team().get_fully_scraped() is False
     assert crappy.get_name() == "Lee Jae-hyeok"
     assert crappy.get_image() == "https://owcdn.net/img/65cc6f0f4da99.png"
     assert crappy.get_status() == PlayerStatus.ACTIVE
-    assert crappy.is_fully_scraped() is True
 
     # Bad player very bad
     assert Player.get_player(None) is None
