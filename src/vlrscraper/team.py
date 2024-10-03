9a684b90084e39762cfab56cb6235c5725a58b0b
Joe <joantpat@gmail.com>
2024-10-03 13:24:35 +0100

    Updated resources to use static constructors depending on the source (#7)

--- src/vlrscraper/team.py
+++ src/vlrscraper/team.py
@@ -14,13 +14,20 @@ class Team:
     resource = Resource("https://vlr.gg/team/<res_id>")
 
     def __init__(
-        self, _id: int, name: str, tag: str, logo: str = "", roster: list[Player] = []
+        self,
+        _id: int,
+        name: Optional[str],
+        tag: Optional[str],
+        logo: Optional[str],
+        roster: Optional[list[Player]],
     ) -> None:
         self.__id = _id
         self.__name = name
         self.__tag = tag
         self.__logo = logo
         self.__roster = roster
+
+        self.__fully_scraped = True
 
     def __eq__(self, other: object) -> bool:
         return (
@@ -35,25 +42,130 @@ class Team:
         return f"{self.get_name()} / {self.get_tag()}, [{self.get_logo()}]"
 
     def get_id(self) -> int:
+        """Get the vlr ID of this team
+
+        Returns
+        -------
+        int
+            vlr ID
+        """
         return self.__id
 
     def get_name(self) -> str:
+        """Get the name of this team
+
+        Returns
+        -------
+        str
+            The name of the team
+        """
         return self.__name
 
     def get_tag(self) -> str:
+        """Get the 1-3 letter team tag of this team
+
+        Returns
+        -------
+        str
+            The team tag
+        """
         return self.__tag
 
     def get_logo(self) -> str:
+        """Get the URL of this team's logo
+
+        Returns
+        -------
+        str
+            The team logo
+        """
         return self.__logo
 
     def get_roster(self) -> list[Player]:
+        """Get the list of players / staff for this team
+
+        Returns
+        -------
+        list[Player]
+            The team roster
+        """
         return self.__roster
 
     def set_roster(self, roster: list[Player]) -> None:
         self.__roster = roster
 
+    def set_fully_scraped(self, scraped: bool) -> None:
+        self.__fully_scraped = scraped
+
+    def get_fully_scraped(self) -> bool:
+        return self.__fully_scraped
+
+    @staticmethod
+    def from_team_page(
+        _id: int, name: str, tag: str, logo: str, roster: list[Player]
+    ) -> Team:
+        """Construct a Team object from the data available on the team's page
+
+        Parameters
+        ----------
+        _id : int
+            The vlr id of the team
+        name : str
+            The full name of the team
+        tag : str
+            The 1-3 letter tag of the team
+        logo : str
+            The url of the team's logo
+        roster : list[Player]
+            List of players and staff on the team
+
+        Returns
+        -------
+        Team
+            The team object created using the given values
+        """
+        team = Team(_id, name, tag, logo, roster)
+        team.set_fully_scraped(True)
+        return team
+
+    @staticmethod
+    def from_player_page(_id: int, name: str, logo: str) -> Team:
+        """Construct a Team object from the data available on a player's page
+
+        Data loaded from the player page: `id`, `name` and `logo`\n
+
+        Parameters
+        ----------
+        _id : int
+            The vlr id of the team
+        name : str
+            The full name of the team
+        logo : str
+            The url of the team's logo
+
+        Returns
+        -------
+        Team
+            The team object created using the given values
+        """
+        team = Team(_id, name=name, tag=None, logo=logo, roster=None)
+        team.set_fully_scraped(False)
+        return team
+
     @staticmethod
     def get_team(_id: int) -> Optional[Team]:
+        """Fetch team data from vlr.gg given the ID of the team
+
+        Parameters
+        ----------
+        _id : int
+            The ID of the team on vlr.gg
+
+        Returns
+        -------
+        Optional[Team]
+            The team data if the ID given was valid, otherwise `None`
+        """
         data = Team.resource.get_data(_id)
         if not data["success"]:
             return None
@@ -73,25 +185,35 @@ class Team:
             f"https:{img}"
             for img in parser.get_elements(const.TEAM_ROSTER_ITEM_IMAGE, "src")
         ]
+        player_tags = [
+            parser.get_text(
+                f"//a[contains(@href, '{p.lower()}')]//div[contains(@class, 'wf-tag')]"
+            )
+            for p in player_aliases
+        ]
 
-        from vlrscraper.player import Player
+        from vlrscraper.player import Player, PlayerStatus
 
-        team = Team(
+        team = Team.from_team_page(
             _id,
             parser.get_text(const.TEAM_DISPLAY_NAME),
             parser.get_text(const.TEAM_TAG),
             f"https:{parser.get_img(const.TEAM_IMG)}",
+            [],
         )
 
         team.set_roster(
             list(
                 [
-                    Player(
+                    Player.from_team_page(
                         pid,
                         player_aliases[i],
-                        team,
                         *player_fullnames[i],
+                        team,
                         image=player_images[i],
+                        status=PlayerStatus.INACTIVE
+                        if player_tags[i] == "Inactive"
+                        else PlayerStatus.ACTIVE,
                     )
                     for i, pid in enumerate(player_ids)
                 ]
